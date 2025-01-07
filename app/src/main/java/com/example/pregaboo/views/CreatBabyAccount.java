6b7694a0fd4d99b5e0b6041e820f89868e409ce6
viranga <virangalakshanz@gmail.com>
2025-01-07 12:19:19 +0530

    create baby profile

--- app/src/main/java/com/example/pregaboo/views/CreatBabyAccount.java
+++ app/src/main/java/com/example/pregaboo/views/CreatBabyAccount.java
@@ -1,36 +1,26 @@
 package com.example.pregaboo.views;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 import com.example.pregaboo.R;
 
-public class UpdateMomDetails extends AppCompatActivity {
+public class CreatBabyAccount extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_update_mom_details);
-        
+        setContentView(R.layout.activity_creat_baby_account);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button addChildButton = findViewById(R.id.addChildButton);
-        addChildButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UpdateMomDetails.this, CreatBabyAccount.class);
-                startActivity(intent);
-            }
         });
     }
 }
